44612fbc4fe16e4a6e0449fb6830c02d4e6a1c3d
P0u4a <pouyamiria@gmail.com>
2022-07-18 22:37:21 +0930

    Add Navbar collapse on select

--- components/navigation.js
+++ components/navigation.js
@@ -8,29 +8,29 @@ import Link from 'next/link';
 export default function Navigate() {
   return (
     <>
-    <Navbar expand='lg' bg='danger' variant='dark' sticky='top'>
-      <Container>
-        <Navbar.Brand href='/'>Wizard</Navbar.Brand>
-        <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
-        <Navbar.Collapse id='responsive-navbar-nav'>
-          <Nav className='me-auto'>
-            <Nav.Item>
-              <Link href='/' passHref>
-                <Nav.Link>Home</Nav.Link>
-              </Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Link href='playlistRequest' passHref>
-                <Nav.Link>Create Playlist</Nav.Link>
-              </Link>
-            </Nav.Item>
-          </Nav>
-          <Nav>
-            <LoginBtn/>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navbar collapseOnSelect expand='lg' bg='danger' variant='dark' sticky ='topic'>
+        <Container>
+          <Navbar.Brand href='/'>Wizard</Navbar.Brand>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Item>
+                <Link href='/' passHref>
+                  <Nav.Link>Home</Nav.Link>
+                </Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Link href='playlistRequest' passHref>
+                  <Nav.Link>Create Playlist</Nav.Link>
+                </Link>
+              </Nav.Item>
+            </Nav>
+            <Nav>
+              <LoginBtn />
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
