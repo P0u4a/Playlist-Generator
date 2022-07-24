fe18993f75f4f86ea32a623a874def4476606348
P0u4a <pouyamiria@gmail.com>
2022-07-24 17:34:40 +0930

    Change slider layout

--- components/range-slider.js
+++ components/range-slider.js
@@ -1,36 +1,26 @@
-// import Tooltip from 'react-bootstrap/Tooltip';
-// import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
 import { useState } from 'react';
 
 export default function Slider() {
   // Keep track of Range slider value
   const [value, setValue] = useState(25);
-  // // Render tooltip on Range slider
-  // const renderTooltip = (props) => (
-  //   <Tooltip id='slider-tooltip' {...props}>
-  //     {value}
-  //   </Tooltip>
-  // );
+  // Render tooltip on Range slider
+  const renderTooltip = (props) => (
+    <Tooltip id='slider-tooltip' {...props}>
+      {value}
+    </Tooltip>
+  );
 
   return (
     <>
-      <Form.Range
-        value={value} onChange={change => setValue(change.target.value)}
-        min={1} max={50}
-      />
-      <Col xs={3}>
-        <Form.Control disabled value={value} />
-      </Col>
+      <OverlayTrigger delay={{ hide: 800 }} placement='right' overlay={renderTooltip}>
+        <Form.Range
+          value={value} onChange={change => setValue(change.target.value)}
+          min={1} max={50}
+        />
+      </OverlayTrigger>
     </>
   );
 }
-
-
-{/* <OverlayTrigger delay={{ hide: 800 }} placement='bottom' overlay={renderTooltip}>
-<Form.Range
-  value={value} onChange={change => setValue(change.target.value)}
-  min={1} max={50}
-/>
-</OverlayTrigger> */}
